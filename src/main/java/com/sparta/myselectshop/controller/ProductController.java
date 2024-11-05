8ba80ff7308bd02a177fff94374d01dec71d6ad9
Hujin0322 <hongyuj0322@naver.com>
2024-11-06 04:45:22 +0900

    폴더 별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -48,10 +48,29 @@ public class ProductController {
         return productService.getProducts(userDetails.getUser(), page-1, size, sortBy, isAsc);
     }
 
-    @PostMapping("/product/{productId}/folder")
+    @PostMapping("/products/{productId}/folder")
     public void addFolder(@PathVariable Long productId,
                           @RequestParam Long folderId,
                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
         productService.addFolder(productId, folderId, userDetails.getUser());
+    }
+
+    @GetMapping("/folders/{folderId}/products")
+    public Page<ProductResponseDto> getProductsInFolder(
+            @PathVariable Long folderId,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") Boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ){
+        return productService.getProductsInFolder(
+                folderId,
+                page-1,
+                size,
+                sortBy,
+                isAsc,
+                userDetails.getUser()
+        );
     }
 }
